a7946842b726e7d1a49e518bbabd821e2d36157c
Jean Y <fyacar_14@hotmail.com>
2024-06-08 14:44:37 -0500

    Iniciando proyecto

---
+++ src/main/java/pe/edu/cibertec/DSWI_CL1_06/DswiCl106Application.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.DSWI_CL1_06;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DswiCl106Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DswiCl106Application.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/cibertec/DSWI_CL1_06/DswiCl106ApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.DSWI_CL1_06;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DswiCl106ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
